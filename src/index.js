aab12209ca739e94daa1b93639ecd6e1d179afe9
Sches <33090199+Sches@users.noreply.github.com>
2018-09-14 22:56:05 +0300

    Solve task

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let k, l;
+  let co = 0;
+  var i;
+  for(i = 0; i < preferences.length; i++)
+  {
+    k = preferences[i]-1;
+    l = preferences[k]-1;
+    if(i === preferences[l]-1)
+      co++;
+  }
+  var count = ~~(co / 3);
+
+  return count;
 };
